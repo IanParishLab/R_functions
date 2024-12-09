9619bacde81ab2277088ec14cf5600e482cce753
yls2g13 <73116184+yls2g13@users.noreply.github.com>
2024-12-09 16:18:56 +1100

    moving things around again

--- distribution_instances.R
+++
@@ -1,6 +0,0 @@
-distribution_instances <- function(df,col_name, nbreaks=10){
-  y <- hist(df[[col_name]], breaks = nbreaks)
-  plot(y, ylim=c(0, max(y$counts)+5))
-  text(y$mids, y$counts+3, y$counts, cex=0.75)
-  
-}
